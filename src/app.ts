617b8b024b0ba9ed3922b76a661ce71ac85c6a3f
Rick Rios <rick.wes1@gmail.com>
2025-02-22 14:31:50 -0300

    refactor: edi app.js cors

--- src/app.ts
+++ src/app.ts
@@ -5,28 +5,25 @@ import { config } from './config/index';
 import { texts } from './utils/textLogs';
 
 dotenv.config();
-// Routers
 import routes from './routers';
 
 const app = express();
 app.use(express.json());
-app.use('/', routes);
 
-// Rota principal
 app.get('/', (req, res) => {
   res.json({ mensagem: 'Bem-vindo ao backend em node!' })
 });
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', config.client);
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, x-access-token'
-  );
-  next();
-});
+app.use(
+  cors({
+    origin: config.client,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'x-access-token'],
+    credentials: true,
+  })
+);
 
-app.use(cors({ origin: texts.localhost }));
+app.use('/', routes);
 
 app.listen(config.port, () => {
    console.log(texts.start_server);
